9280e9af0fcee4512d4f4e647f0d6e62edae3b56
jeff147 <jre5791@gmail.com>
2019-10-17 23:05:03 -0400

    More switch statements

--- RyanCalculator/OrderForm.cs
+++ RyanCalculator/OrderForm.cs
@@ -96,6 +96,150 @@ namespace RyanCalculator
                         break;
                 }
             }
+            else if (quantity >= 49 && quantity <= 96)
+            {
+                switch (numColors)
+                {
+                    case 1:
+                        price = 1.00;
+                        break;
+                    case 2:
+                        price = 1.50;
+                        break;
+                    case 3:
+                        price = 2.00;
+                        break;
+                    case 4:
+                        price = 2.50;
+                        break;
+                    case 5:
+                        price = 3.00;
+                        break;
+                    case 6:
+                        price = 3.50;
+                        break;
+                }
+            }
+            else if (quantity >=97 && quantity <= 144)
+            {
+                switch (numColors)
+                {
+                    case 1:
+                        price = .95;
+                        break;
+                    case 2:
+                        price = 1.35;
+                        break;
+                    case 3:
+                        price = 1.80;
+                        break;
+                    case 4:
+                        price = 2.20;
+                        break;
+                    case 5:
+                        price = 2.60;
+                        break;
+                    case 6:
+                        price = 3.00;
+                        break;
+                }
+            }
+            else if (quantity >= 145 && quantity <= 249)
+            {
+                switch (numColors)
+                {
+                    case 1:
+                        price = .80;
+                        break;
+                    case 2:
+                        price = 1.10;
+                        break;
+                    case 3:
+                        price = 1.40;
+                        break;
+                    case 4:
+                        price = 1.70;
+                        break;
+                    case 5:
+                        price = 2.00;
+                        break;
+                    case 6:
+                        price = 2.30;
+                        break;
+                }
+            }
+            else if (quantity >= 250 && quantity <= 499)
+            {
+                switch (numColors)
+                {
+                    case 1:
+                        price = .70;
+                        break;
+                    case 2:
+                        price = .80;
+                        break;
+                    case 3:
+                        price = .92;
+                        break;
+                    case 4:
+                        price = 1.10;
+                        break;
+                    case 5:
+                        price = 1.25;
+                        break;
+                    case 6:
+                        price = 1.40;
+                        break;
+                }
+            }
+            else if (quantity >= 500 && quantity <= 999)
+            {
+                switch (numColors)
+                {
+                    case 1:
+                        price = .57;
+                        break;
+                    case 2:
+                        price = .75;
+                        break;
+                    case 3:
+                        price = .87;
+                        break;
+                    case 4:
+                        price = .96;
+                        break;
+                    case 5:
+                        price = 1.06;
+                        break;
+                    case 6:
+                        price = 1.15;
+                        break;
+                }
+            }
+            else if (quantity >= 1000 && quantity <= 1999)
+            {
+                switch (numColors)
+                {
+                    case 1:
+                        price = .51;
+                        break;
+                    case 2:
+                        price = .62;
+                        break;
+                    case 3:
+                        price = .75;
+                        break;
+                    case 4:
+                        price = .84;
+                        break;
+                    case 5:
+                        price = .93;
+                        break;
+                    case 6:
+                        price = 1.05;
+                        break;
+                }
+            }
             txtResult.Text = price.ToString();
 
 
